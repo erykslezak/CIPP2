25f7ea451a69a4474bcaad678b4303ce87a864a0
erykslezak <erykslezak@outlook.ie>
2021-11-25 00:29:04 +0000

    add html variables in js and game array

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Variables to get all the HTML elements
+let choiceDisplay = document.getElementById("message");
+let resultDisplay = document.getElementById("result");
+let buttons = document.querySelectorAll("button");
+
+// necessary basic choices array
+let choices = ['rock', 'paper', 'scissors'];
